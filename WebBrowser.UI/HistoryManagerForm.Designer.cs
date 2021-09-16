47d43672e49aa09be954ed31766776ea6fab20f0
leticia825 <lzg0052@auburn.edu>
2021-09-16 17:58:17 -0500

    Created HistoryManagerForm

---
+++ WebBrowser.UI/HistoryManagerForm.Designer.cs
@@ -0,0 +1,61 @@
+﻿
+namespace WebBrowser.UI
+{
+    partial class HistoryManagerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.HistoryListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // HistoryListBox
+            // 
+            this.HistoryListBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.HistoryListBox.FormattingEnabled = true;
+            this.HistoryListBox.ItemHeight = 16;
+            this.HistoryListBox.Location = new System.Drawing.Point(0, 0);
+            this.HistoryListBox.Name = "HistoryListBox";
+            this.HistoryListBox.Size = new System.Drawing.Size(800, 450);
+            this.HistoryListBox.TabIndex = 0;
+            // 
+            // HistoryManagerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.HistoryListBox);
+            this.Name = "HistoryManagerForm";
+            this.Text = "HistoryManagerForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox HistoryListBox;
+    }
+}
